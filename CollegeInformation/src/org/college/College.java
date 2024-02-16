d296ccf61b6c25638f46da055b09f8785f9b9aa1
kirupaa96 <kirupaashankar@192.168.0.27>
2024-02-15 20:59:26 -0500

    Testing new token

--- CollegeInformation/src/org/college/College.java
+++ CollegeInformation/src/org/college/College.java
@@ -1,8 +1,7 @@
 package org.college;
 
 public class College {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		College col = new College();
 		col.collegeName();
 		col.collegeCode();
